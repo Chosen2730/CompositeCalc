0aebbcf67491d8123a2b547c012807c26bca9f01
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-05-07 01:22:27 +0100

    adjustmed some functions

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import React, {useState} from 'react';
-import './App.css';
-import Heading from './header';
-import Form from './form'
+import React, { useState, useRef } from "react";
+import "./App.css";
+import Heading from "./header";
+import Form from "./form";
 
-const App = ()=> {
-const [inputText, setInputText]=useState("User");
-const [utmeScore, setUTMEScore]=useState("");
-const [postUTMEScore, setPostUTMEScore]=useState("");
-const [gradeValue1, setGradeValue1]=useState(0);
-const [gradeValue2, setGradeValue2]=useState(0);
-const [gradeValue3, setGradeValue3]=useState(0);
-const [gradeValue4, setGradeValue4]=useState(0);
-const [gradeValue5, setGradeValue5]=useState(0);
-const [DEvalue, setDEvalue]=useState(0);
-const [DEinput, setDEinput]=useState(0);
-
-const [composite, setComposite]=useState(0);
+const App = () => {
+  const [category, setCategory] = useState("");
+  const compositeContainer = useRef(null);
+  const [user, setUser] = useState({
+    name: "",
+    examCategory: "",
+    mathsValue: 0,
+    englishValue: 0,
+    biologyValue: 0,
+    physicsValue: 0,
+    chemValue: 0,
+    putmeValue: 0,
+    utmeValue: 0,
+    DETotal: 0,
+    DEValue: 0,
+    composite: 0,
+    showComposite: false,
+  });
 
   return (
-  <>
-  <Heading/>
-  <Form 
-  setInputText={setInputText} 
-  inputText={inputText}
-  utmeScore ={utmeScore}
-  setUTMEScore = {setUTMEScore}
-  postUTMEScore = {postUTMEScore}
-  setPostUTMEScore = {setPostUTMEScore}
-  gradevalue1 = {gradeValue1}
-  setGradeValue1 = {setGradeValue1}
-  gradevalue2 = {gradeValue2}
-  setGradeValue2 = {setGradeValue2}
-  gradevalue3 = {gradeValue3}
-  setGradeValue3 = {setGradeValue3}
-  gradevalue4 = {gradeValue4}
-  setGradeValue4 = {setGradeValue4}
-  gradevalue5 = {gradeValue5}
-  setGradeValue5 = {setGradeValue5}
-  composite = {composite}
-  setComposite = {setComposite}
-  DEinput = {DEinput}
-  setDEinput = {setDEinput}
-  DEvalue = {DEvalue}
-  setDEvalue = {setDEvalue} 
-  />
-  </>
+    <>
+      <Heading />
+      <Form
+        setCategory={setCategory}
+        category={category}
+        user={user}
+        setUser={setUser}
+        compositeContainer={compositeContainer}
+      />
+    </>
   );
-}
+};
 
 export default App;
--- src/form.js
+++ src/form.js
@@ -1,191 +1,232 @@
 import React from "react";
 import SelectGrade from "./Gradeselect";
 
-const form =({subject, setInputText, inputText, 
-    utmeScore, setUTMEScore, postUTMEScore, 
-    setPostUTMEScore, 
-    gradevalue1, setGradeValue1, 
-    gradevalue2, setGradeValue2, 
-    gradevalue3, setGradeValue3, 
-    gradevalue4, setGradeValue4, 
-    gradevalue5, setGradeValue5, 
-    composite, setComposite,
-    DEvalue, setDEvalue,
-    DEinput, setDEinput})=>{
-
-
-    const inputHandler = (e)=>{
-        setInputText(e.target.value);
+const Form = ({ setCategory, category, user, setUser, compositeContainer }) => {
+  const {
+    name,
+    examCategory,
+    putmeValue,
+    utmeValue,
+    DETotal,
+    DEValue,
+    mathsValue,
+    englishValue,
+    biologyValue,
+    physicsValue,
+    chemValue,
+    composite,
+    showComposite,
+  } = user;
+  const changeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+  const inputHandler = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  const calculate = (e) => {
+    e.preventDefault();
+    const allValues = [
+      mathsValue,
+      chemValue,
+      englishValue,
+      physicsValue,
+      biologyValue,
+      putmeValue,
+      utmeValue,
+      DETotal,
+      DEValue,
+    ];
+    const values = allValues.map((value) => {
+      return Number(value);
+    });
+    const [m, c, en, p, b, putme, utme, dt, d] = values;
+    const OlevelTotal = ((m + c + en + p + b) / 30) * 20;
+    const utmeTotal = utme / 8;
+    const putmeTotal = (putme / 100) * 30;
+    const deTotal = (d / dt) * 50;
+    if (category === "DE") {
+      const total = (OlevelTotal + putmeTotal + deTotal).toFixed(2);
+      setUser({ ...user, composite: total, showComposite: true });
+    } else if (category === "UTME") {
+      const total = (OlevelTotal + utmeTotal + putmeTotal).toFixed(2);
+      setUser({ ...user, composite: total, showComposite: true });
+    } else {
+      setUser({ ...user, composite: 0, showComposite: false });
     }
-    const utmeInput = (e)=>{
-        setUTMEScore(e.target.value);
-    }
-    const postUtmeInput = (e)=>{
-        setPostUTMEScore(e.target.value)
-    }
-    const calcGrade = ()=>{
-        const Olevel = ((Number(gradevalue1) + Number(gradevalue2) + Number(gradevalue3) + 
-        Number(gradevalue4) + Number(gradevalue5))/30)*20;
-        const UTME = Number(utmeScore)/8;
-        const PUTME = ((Number(postUTMEScore))/100)*30;
-        // const DEVALUE = Number((DEvalue/DEinput)*50);
-        return (UTME + PUTME + Olevel).toFixed(2);
-    }
-    const clickHandler = e =>{
-        e.preventDefault();
-        calcGrade();
-        setComposite(calcGrade);
-    }
-    const clearHandler = e =>{
-        // e.preventDefault();
-        // // setUTMEScore(0);
-        // // setPostUTMEScore(0);
-        // // setInputText("User");
-        // // setGradeValue1(0);
-        // // setGradeValue2(0);
-        // // setGradeValue3(0);
-        // // setGradeValue4(0);
-        // // setGradeValue5(0);
-        // // setComposite(0);
-    }
-    const gradeAction = e=>{
-       setGradeValue1(e.target.value);
-    }
-    const gradeAction1 = e=>{
-        setGradeValue2(e.target.value);
-     }
-     const gradeAction2 = e=>{
-        setGradeValue3(e.target.value);
-     }
-     const gradeAction3 = e=>{
-        setGradeValue4(e.target.value);
-     }
-     const gradeAction4 = e=>{
-        setGradeValue5(e.target.value);
-     }
-    //  const DEaction = e =>{
-    //      setDEvalue(e.target.value);
-    //  }
-    //  const DEinputHandler=e=>{
-    //      setDEinput(e.target.value);
-    //  }
-     
-    return(
-        <>
-        <form>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>Username</label>
-                </div>
-                <div className='form-col input'>
-                    <input placeholder='Enter Username' onChange={inputHandler} type='text'></input>
-                </div>
-            </div>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>Exam Type</label>
-                </div>
-                <div className='form-col input'>
-                   <select>
-                       <option>WAEC</option>
-                       <option>NECO</option>
-                   </select>
-                </div>
-            </div>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label className='selectLabel'>Mathematics</label>
-                </div>
-                <div className='form-col input'>
-                <select onChange={gradeAction} >
-                       <SelectGrade/>
-                   </select>
-                </div>   
-            </div>
-            <div className='form-flex'>
+  };
+  return (
+    <>
+      <form>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label htmlFor='userName'>Username</label>
+          </div>
+          <div className='form-col input'>
+            <input
+              placeholder='Enter Username'
+              name='name'
+              type='text'
+              id='userName'
+              value={name}
+              onChange={inputHandler}
+            ></input>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label htmlFor='type'>Exam Type</label>
+          </div>
+          <div className='form-col input'>
+            <select
+              id='type'
+              onChange={inputHandler}
+              name='examCategory'
+              value={examCategory}
+            >
+              <option>WAEC</option>
+              <option>NECO</option>
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>Mathematics</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={inputHandler} name='mathsValue'>
+              <SelectGrade />
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>English</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={inputHandler} name='englishValue'>
+              <SelectGrade />
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>Physics</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={inputHandler} name='physicsValue'>
+              <SelectGrade />
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>Biology</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={inputHandler} name='biologyValue'>
+              <SelectGrade />
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>Chemistry</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={inputHandler} name='chemValue'>
+              <SelectGrade />
+            </select>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label>Post UTME score</label>
+          </div>
+          <div className='form-col input'>
+            <input
+              placeholder={0}
+              type='number'
+              onChange={inputHandler}
+              name='putmeValue'
+            ></input>
+          </div>
+        </div>
+        <div className='form-flex'>
+          <div className='form-col label'>
+            <label className='selectLabel'>Select Category</label>
+          </div>
+          <div className='form-col input'>
+            <select onChange={changeCategory}>
+              <option>UTME/DE</option>
+              <option value='UTME'>UTME</option>
+              <option value='DE'>DE</option>
+            </select>
+          </div>
+        </div>
+        {category === "UTME" ? (
+          <div className='form-flex'>
             <div className='form-col label'>
-                    <label className='selectLabel'>English</label>
-                </div>
-                <div className='form-col input'>
-                <select onChange={gradeAction1} >
-                <SelectGrade/>
-                   </select>
-                </div>
+              <label>UTME Score</label>
+            </div>
+            <div className='form-col input'>
+              <input
+                placeholder={0}
+                type='number'
+                onChange={inputHandler}
+                name='utmeValue'
+              ></input>
+            </div>
+          </div>
+        ) : category === "DE" ? (
+          <>
+            <div className='form-flex'>
+              <div className='form-col label'>
+                <label>Grade Total/Points obtainable</label>
+              </div>
+              <div className='form-col input'>
+                <input
+                  placeholder={"e.g., 4.0"}
+                  type='number'
+                  onChange={inputHandler}
+                  name='DETotal'
+                ></input>
+              </div>
             </div>
             <div className='form-flex'>
-            <div className='form-col label'>
-                    <label className='selectLabel'>Physics</label>
-                </div>
-                <div className='form-col input'>
-                <select onChange={gradeAction2} >
-                <SelectGrade/>
-                   </select>
-                </div>
+              <div className='form-col label'>
+                <label>CGPA/Points Obtained</label>
+              </div>
+              <div className='form-col input'>
+                <input
+                  type='number'
+                  onChange={inputHandler}
+                  name='DEValue'
+                ></input>
+              </div>
             </div>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label className='selectLabel'>Biology</label>
-                </div>
-                <div className='form-col input'>
-                <select onChange={gradeAction3} >
-                <SelectGrade/>
-                   </select>
-                </div>
-            </div>
-            <div className='form-flex'>
-            <div className='form-col label'>
-                    <label className='selectLabel'>Chemistry</label>
-                </div>
-                <div className='form-col input'>
-                <select onChange={gradeAction4} >
-                <SelectGrade/>
-                   </select>
-                </div>
-            </div>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>Post UTME score</label>
-                </div>
-                <div className='form-col input'>
-                    <input placeholder={0} onChange={postUtmeInput} type='number'></input>
-                </div>
-            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        {category && (
+          <div className='submit'>
+            <button onClick={calculate}>Calculate Composite</button>
+            {showComposite && (
+              <h3 className='result'>
+                Hello {name}, your composite is{" "}
+                <span ref={compositeContainer}>{composite}</span> points.
+              </h3>
+            )}
+          </div>
+        )}
+      </form>
+    </>
+  );
+};
 
-            {/* <h1 className='category'>For UTME candidates Only</h1> */}
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>UTME Score</label>
-                </div>
-                <div className='form-col input'>
-                    <input placeholder={0}  onChange={utmeInput} type='number'></input>
-                </div>
-            </div>
-
-            {/* <h1 className='category'>For Direct Entry candidates Only</h1>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>Grade Total/Points obtainable</label>
-                </div>
-                <div className='form-col input'>
-                <input placeholder={"4.0"} type='number' onChange={DEaction}></input>
-                </div>
-            </div>
-            <div className='form-flex'>
-                <div className='form-col label'>
-                    <label>CGPA/Points Obtained</label>
-                </div>
-                <div className='form-col input'>
-                    <input onChange={DEinputHandler} type='number'></input>
-                </div>
-            </div> */}
-            <div className='submit'>
-            <button onClick={clickHandler}>Calculate Composite</button> 
-            <button className="clear" onClick={clearHandler}>Clear</button>
-            <h3 className='result'>Hello {inputText}, your composite is <span> {composite} points.</span></h3>
-            </div>
-        </form>
-        </>
-    )
-}
-
-export default form;
+export default Form;
