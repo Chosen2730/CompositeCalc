f7c993dc70bcdec713e8df3d19e4aa800698e6e0
Robinson Simon <cf.youvarsity@gmail.com>
2023-08-01 00:58:37 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ const App = () => {
     <div className='p-5 py-20'>
       <div className='max-w-4xl mx-auto'>
         <Heading />
+        <div className='bg-gray-900 bg-opacity-60 text-white text-sm flex flex-col gap-2 rounded-md p-3 my-5'>
+          <h2 className='font-bold text-lg'>How it works</h2>
+          <li className='list-disc'>O level Result: 30 points</li>
+          <li className='list-disc'>UTME Result: 60 points</li>
+          <li className='list-disc'>One Sitting: 10 points</li>
+          <li className='list-disc'>Two Sitting: 6 points</li>
+        </div>
         <Form />
+        <p className='text-white text-center text-sm'>
+          Built by{" "}
+          <a
+            href='https://wa.me/2348132157321'
+            className='text-blue-500 font-bold'
+          >
+            Chosen Foundation
+          </a>{" "}
+          for FUNAAB Aspirants - Always choose Chosen
+        </p>
       </div>
     </div>
   );
